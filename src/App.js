960e98167749bb8c7f758ee8e31e26f39bdf7eab
Abraham Garcia <GingerbreadMan@Abrahams-MacBook-Air.local>
2021-09-10 16:15:05 -0700

    got some text without styling

--- src/App.js
+++ src/App.js
@@ -1,32 +1,31 @@
-import React, { useEffect, useState }from 'react';
 import './App.css';
-import Axios from 'axios';
-
-
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import Character from './components/Character';
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-  const [characters, setCharacters] = useState('')
-  useEffect( () => {
-    Axios.get('https://swapi.dev/api/people').then((resp)=>setCharacters(resp)).catch(<h1>'Loading'</h1>)
-  }, []);
-  console.log(characters)
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-  const name = characters.data[0].name
-=======
->>>>>>> b5bd72a9e4ecd72982458a05194853e5239f7027
->>>>>>> b2d735f (got luke name on screen)
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
-
+const [starWarsData, setStarWarsData] = useState([])
+const [error, setError] = useState(null);
+useEffect(() => {
+  axios.get('https://swapi.dev/api/people/')
+    .then(res => {
+      setStarWarsData(res.data)
+    })
+    .catch(err => {
+      setError(`Please try again later`) 
+    })
+  }, [])
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <h1 className="Header">Star Wars Characters</h1>
+      {starWarsData.map(item => {
+        return <Character data={item} />
+      })}
     </div>
   );
 }
-
 export default App;
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,24 @@
 // Write your Character component here
+import react from "react"
+
+const Character = (props) => {
+	const {data}=props
+		return (
+			<div>
+				<div className='name'>
+					<h2>{data.name}</h2>
+				</div>
+			<div className='info'>
+				<p>Height: {data.height}</p>
+				<p>Mass: {data.mass}</p>
+				<p>Hair color: {data.hair_color}</p>
+				<p>Skin Color: {data.skin_color}</p>
+				<p>Birth Year: {data.birth_year}</p>
+				<p>Eye Color: {data.eye_color}</p>
+				<p>Birth Year: {data.birth_year}</p>
+				<p>Gender: {data.gender}</p>
+			</div>
+			</div>
+		)
+	 }
+export default Character
